0900c9cadc4e0ee0f87e97e810dd0ec3fdd0b760
freddotclem <f.clems@gmail.com>
2024-12-09 22:18:53 +0100

    setting python

---
+++ cicd/setup.sh
@@ -0,0 +1,13 @@
+python3 --version
+
+sudo apt update
+sudo apt install python3 python3-pip
+
+python3 -m venv successfactors-env
+
+source successfactors-env/bin/activate
+
+# pip install requests
+# pip freeze > requirements.txt
+
+pip install -r requirements.txt
